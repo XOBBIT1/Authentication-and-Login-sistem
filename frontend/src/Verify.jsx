295245e3beae14492a2fcb5b94ad3d560ea14b2c
Денис Гришанков <ben300300@gmail.com>
2023-04-07 19:30:41 +0300

    minor changes

--- frontend/src/Verify.jsx
+++ frontend/src/Verify.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import { ToastContainer } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
@@ -9,17 +9,28 @@ export const Verify = (props) => {
 
     const handleSubmit = (event) =>{
         event.preventDefault();
-
     }
+    const notify = () => {
+        toast.success('You verified successfully! ', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            },)
+    }
+
     return ( 
         <div>
              <form className="login-form" onSubmit={handleSubmit}>
                 <label form="token">token</label>
                 <input className="token_input" value={verify} onChange={(event) => setToken(event.target.value)} typr="token" placeholder="your token" id="verify" name="verify" />
-                <button className="verify-btn" types="submit">Confirm Account</button>
+                <button className="verify-btn" types="submit" onClick={notify}>Confirm Account</button>
                 <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Go to Login</button>
             </form>
-            <ToastContainer/>
         </div>
     )
 }
